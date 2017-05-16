b322031183edfbcc344a34c944c7e08f50f47483
Daniel Endres <danielendres@gmail.com>
2017-05-16 10:32:31 -0700

    add Epicenter controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tweets
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
 end
